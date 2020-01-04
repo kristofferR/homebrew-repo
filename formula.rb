9fef4d3ccd81f6fcf883bf1b39b4ddc96e1a234c
KristofferR <kristoffer@risanger.no>
2020-01-04 11:15:17 +0100

    Create formula.rb

---
+++ formula.rb
@@ -0,0 +1,22 @@
+class YoutubeDl-date < Formula
+  desc "Download YouTube videos from the command-line"
+  homepage "https://github.com/kade-robertson/youtube-dl"
+  url "https://github.com/kade-robertson/youtube-dl/archive/master.zip"
+
+  head do
+    url "https://github.com/kade-robertson/youtube-dl.git"
+    depends_on "pandoc" => :build
+  end
+
+  bottle :unneeded
+
+  def install
+    system "make", "PREFIX=#{prefix}" if build.head?
+    bin.install "youtube-dl-date"
+  end
+
+  test do
+    system "#{bin}/youtube-dl-date", "--simulate", "https://www.youtube.com/watch?v=he2a4xK8ctk"
+    system "#{bin}/youtube-dl-date", "--simulate", "--yes-playlist", "https://www.youtube.com/watch?v=7FlzFH9xOF8&list=PLRuS3NbVmMIq4XypJdggVfUVJNRZjbfCR"
+  end
+end
